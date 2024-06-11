fab93c30faa51672ce551eb59eacebbaf29af606
claudia <claudiah@dakota.com.br>
2024-06-11 13:04:44 -0300

    Adiciona modal para cadastro de usuario

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbModalConfig, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+
+  private modalReference?: NgbModalRef;
+
+  constructor(
+		config: NgbModalConfig,
+		private modalService: NgbModal,
+	) {
+		config.backdrop = 'static';
+		config.keyboard = false;
+	}
+
+  open(content: any) {
+    this.modalReference = this.modalService.open(content, { centered: true });
+  }
+
+  close() {
+    if (confirm("Deseja sair sem salvar ?")) {
+      this.modalReference?.close();
+    }
+  }
 
 }
